4d296bff8a2139e7f6e5c90e0daa6d44d29e6b5b
Mourad Ben Fradj <mourad.ben.fradj@gmail.com>
2025-02-16 17:18:21 +0100

    for db

--- public/migrate.php
+++ public/migrate.php
@@ -1,8 +1,14 @@
 <?php
-// public/migrate.php
 
-// Mot de passe simple pour sécuriser l'accès
-$password = 'TonMotDePasseUltraSecurise';
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+
+use Symfony\Component\Dotenv\Dotenv;
+// Charge les composants Symfony et exécute les migrations
+require __DIR__ . '/../vendor/autoload.php';
+
+$password = 'MBF6mm09761130';
 
 // Vérifie le mot de passe
 if (!isset($_GET['password']) || $_GET['password'] !== $password) {
@@ -11,18 +17,10 @@ if (!isset($_GET['password']) || $_GET['password'] !== $password) {
     exit;
 }
 
-// Charge les composants Symfony et exécute les migrations
-require __DIR__ . '/../vendor/autoload.php';
-
-use Symfony\Component\Dotenv\Dotenv;
 
 // Charge les variables d'environnement
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/../.env.local');
-
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
 
 $kernel = new \App\Kernel('prod', false);
 $kernel->boot();
@@ -32,8 +30,8 @@ $application->setAutoExit(false);
 
 // Définir la variable d'environnement pour exclure la migration
 putenv('SKIP_MIGRATION_20231021152830=true');
-
-echo nl2br($outputMigrate->fetch());
+/* 
+echo nl2br($outputMigrate->fetch()); */
 
 // Exécute la commande doctrine:schema:update
 $inputSchemaUpdate = new ArrayInput([
