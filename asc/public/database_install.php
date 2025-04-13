328a7954e5334d6cca4034a18f631689e598bc21
Mourad Ben Fradj <mourad.ben.fradj@gmail.com>
2025-04-13 01:49:43 +0100

    githubactino

--- asc/public/database_install.php
+++ asc/public/database_install.php
@@ -34,6 +34,7 @@ $application->setAutoExit(false);
 // Exécute la commande doctrine:schema:update
 $inputSchemaUpdate = new ArrayInput([
     'command' => 'doctrine:schema:update',
+    '--complete' => true,
     '--force' => true,
     '--dump-sql' => true,
     '--no-interaction' => true,
