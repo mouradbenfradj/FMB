3472d6e634ebf5dd911554a19d4465fcec23e06c
Mourad Ben Fradj <mourad.ben.fradj@gmail.com>
2025-11-16 11:08:02 +0100

    formulaire admin user

--- asc/src/Admin/UserAdmin.php
+++ asc/src/Admin/UserAdmin.php
@@ -9,6 +9,12 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class UserAdmin extends AbstractAdmin
 {
@@ -66,20 +72,40 @@ final class UserAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $form): void
     {
         $form
-            ->add('username')
-            ->add('usernameCanonical')
-            ->add('email')
-            ->add('emailCanonical')
-            ->add('enabled')
-            ->add('salt')
-            ->add('password')
-            ->add('lastLogin')
-            ->add('confirmationToken')
-            ->add('passwordRequestedAt')
-            ->add('roles')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('id')
+            ->with('Informations principales')
+                ->add('username', TextType::class, [
+                    'label' => 'Nom d\'utilisateur',
+                ])
+                ->add('email', EmailType::class, [
+                    'label' => 'Email',
+                ])
+                ->add('enabled', CheckboxType::class, [
+                    'label' => 'Compte actif',
+                    'required' => false,
+                ])
+            ->end()
+
+            ->with('Mot de passe')
+                ->add('plainPassword', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'required' => $this->isCurrentRoute('create'),
+                    'first_options'  => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Confirmation du mot de passe'],
+                ])
+            ->end()
+
+            ->with('Rôles')
+                ->add('roles', ChoiceType::class, [
+                    'label' => 'Rôles',
+                    'choices' => [
+                        'Utilisateur' => 'ROLE_USER',
+                        'Administrateur' => 'ROLE_ADMIN',
+                        'Super administrateur' => 'ROLE_SUPER_ADMIN',
+                    ],
+                    'multiple' => true,
+                    'expanded' => true,
+                ])
+            ->end()
         ;
     }
 
